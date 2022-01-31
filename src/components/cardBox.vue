5b7c22750c47f184923a713716791a2f82996f36
mattia laurenzana <mattilaure@gmail.com>
2022-01-31 17:11:46 +0100

    completamento sezione dischi

--- src/components/cardBox.vue
+++ src/components/cardBox.vue
@@ -1,7 +1,10 @@
 <template>
     
         <div class="card">
-            
+            <img :src="disk.poster" alt="music disk">
+            <div class="title">{{disk.title}}</div>
+            <div class="author">{{disk.author}}</div>
+            <span class="year">{{disk.year}}</span>
         </div>
     
 </template>
@@ -15,5 +18,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .card{
+        background-color:  rgb(54, 65, 77);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 10px 0;
+        font-size: 11px;
+        margin: 10px 10px;
+        width: calc((20%) - 20px);
 
+        img{
+            width: 100px;
+            height: 100px;
+        }
+
+        .title{
+            font-size: 16px;
+            color: white;
+            text-transform: uppercase;
+            max-width: 95px;
+            word-break: break-word;
+            text-align: center;
+            margin: 5px 0;
+        }
+
+        .author, .year{
+            font-size:15px;
+            color: grey;
+        }
+
+    }
 </style>
--- src/components/mainContainer.vue
+++ src/components/mainContainer.vue
@@ -22,5 +22,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .container{
+    
+        padding: 40px;
 
+    
+    }
 </style>
